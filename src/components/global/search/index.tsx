cbc69fd1cb3365bc2f801de117aaeb4a65a949b9
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-04-12 20:29:06 -0400

    Testing user search UI with mock data and layout enhancements

--- src/components/global/search/index.tsx
+++ src/components/global/search/index.tsx
@@ -3,6 +3,8 @@ import { useSearch } from '@/hooks/useSearch'
 import { useMutationData } from '@/hooks/useMutationData'
 import { Input } from '@/components/ui/input'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { User } from 'lucide-react'
 type Props = {
     workspaceId: string
 }
@@ -26,14 +28,35 @@ return (
      placeholder= " Search for your user.."
      type="text"
      />
+      <div>
+                {[{id: 'asdfrgt',  image:"asdfhyt", firstname: "Anirban", lastname: "Dey"}].map((user) => (
+                    <div key={user.id} className='flex gap-x-3 items-center border-2 w-full p-3 rounded-xl'>
+                        <Avatar>
+                            <AvatarImage src={user.image as string}/>
+                            <AvatarFallback>
+                                <User />
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className='flex flex-col items-start'>
+                            <h3 className= "text-bold text-lg capitalize">{user.firstname} {user.lastname}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
      {isFetching ? (
         <div className='flex flex-col gap-y-2'>
             <Skeleton className='w-full h-8 rounded-xl'/>
         </div> 
      ) : !onUsers ? (
         <p className="text-center text-sm text-[#a4a4a4]">No users found</p>
-        )   : (
-            ''
+        )   : ( null
+            // <div>
+            //     {onUsers.map((user) => (
+            //         <div key={user.id} className='flex gap-x-3 items-center border-2 w-full p-3 rounded-xl'>
+                        
+            //         </div>
+            //     ))}
+            // </div>
          )
      
         
