f58a511013226ea03660f7f7772c8221b72ffcca
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-04-12 21:01:41 -0400

    Added invite button to user search UI with subscription display

--- src/components/global/search/index.tsx
+++ src/components/global/search/index.tsx
@@ -5,6 +5,8 @@ import { Input } from '@/components/ui/input'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { User } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import Loader from '../loader'
 type Props = {
     workspaceId: string
 }
@@ -16,6 +18,7 @@ const Search = ({workspaceId}: Props) => {
    
 
 //WIP: Wired to invite member
+//WIP: Invite Button in UI
 // const {mutate, isPending} = useMutationData(['invite-member'], 
 //     (data: {recieverId: string; email: string })=> {
 
@@ -28,35 +31,35 @@ return (
      placeholder= " Search for your user.."
      type="text"
      />
-      <div>
-                {[{id: 'asdfrgt',  image:"asdfhyt", firstname: "Anirban", lastname: "Dey"}].map((user) => (
-                    <div key={user.id} className='flex gap-x-3 items-center border-2 w-full p-3 rounded-xl'>
-                        <Avatar>
-                            <AvatarImage src={user.image as string}/>
-                            <AvatarFallback>
-                                <User />
-                            </AvatarFallback>
-                        </Avatar>
-                        <div className='flex flex-col items-start'>
-                            <h3 className= "text-bold text-lg capitalize">{user.firstname} {user.lastname}</h3>
-                        </div>
-                    </div>
-                ))}
-            </div>
+     
      {isFetching ? (
         <div className='flex flex-col gap-y-2'>
             <Skeleton className='w-full h-8 rounded-xl'/>
         </div> 
      ) : !onUsers ? (
         <p className="text-center text-sm text-[#a4a4a4]">No users found</p>
-        )   : ( null
-            // <div>
-            //     {onUsers.map((user) => (
-            //         <div key={user.id} className='flex gap-x-3 items-center border-2 w-full p-3 rounded-xl'>
-                        
-            //         </div>
-            //     ))}
-            // </div>
+        )   : ( 
+            <div>
+                {onUsers.map((user) => (
+                    <div key={user.id} className='flex gap-x-3 items-center border-2 w-full p-3 rounded-xl'>
+                    <Avatar>
+                        <AvatarImage src={user.image as string}/>
+                        <AvatarFallback>
+                            <User />
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className='flex flex-col items-start'>
+                        <h3 className= "text-bold text-lg capitalize">{user.firstname} {user.lastname}</h3>
+                        <p className='lowercase text-xs bg-white px-2 rounded-lg text-[#1e1e1e]'>{user.subscription?.plan}</p>
+                    </div>
+                    <div className="flex-1 flex justify-end items-center">
+                        <Button onClick={()=> {}} variant= {"default"} className="w-5/12 font-bold">
+                            <Loader state={false} color="#000">Invite</Loader>
+                        </Button>
+                    </div>
+                </div>
+                ))}
+            </div>
          )
      
         
