5df08a406476e7fe2181f6068c2df08c6e8dca91
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-04-15 18:53:51 -0400

    Implement Sidebar and InfoBar components - Add search, upload and record buttons in InfoBar - Update Sidebar styling with proper layout, colors and mobile responsiveness

--- src/components/global/info-bar/index.tsx
+++ src/components/global/info-bar/index.tsx
@@ -1,19 +1,37 @@
 'use client'
 
+import VideoRecorderIcon from "@/components/icons/video-recorder"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Search } from "lucide-react"
+import { UserButton } from "@clerk/nextjs"
+import { Search, UploadIcon } from "lucide-react"
 
 type Props = {}
 
 const InfoBar = (props: Props) => {
     return (
         <header className="pl-20 md:pl-[265px] fixed p-4 w-full flex items-center justify-between gap-4">
-            <div className="flex gap-2 items-center bg-[#111111] border border-neutral-800 rounded-lg px-4 py-2 w-full max-w-lg">
-                <Search size={20} className="text-neutral-500" />
+            <div className="flex gap-4 justify-center items-center border-2  rounded-full px-4 w-full max-w-lg">
+                <Search size={25} className="text-[#707070]" />
                 <Input 
                     className="bg-transparent border-none text-sm text-neutral-400 placeholder:text-neutral-500 focus-visible:ring-0 px-0"
-                    placeholder="Search for people, projects"
+                    placeholder="Search for people, projects, tags & folders"
                 />
+            </div>
+            <div className="flex items-center gap-4">
+                <Button className="bg-[#9D9D9D] flex items-cenrer gap-2">
+                    <UploadIcon size={20} /> {' '}
+                    <span className="flex items-center gap-2">
+                        Upload
+                    </span>
+                </Button>
+                <Button className="bg-[#9D9D9D] flex items-cenrer gap-2">
+                    <VideoRecorderIcon  />  
+                    <span className="flex items-center gap-2">
+                        Record
+                    </span>
+                </Button>
+                <UserButton />
             </div>
         </header>
     )
