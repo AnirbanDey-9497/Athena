df0a0f26174bf31142f0004360b873eff4fd10f1
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-05-04 18:02:11 -0400

    Refractored the edit button for best user experience

--- src/components/global/videos/edit.tsx
+++ src/components/global/videos/edit.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import Modal from '../modal'
+import { Button } from '@/components/ui/button'
+import { Edit } from 'lucide-react'
+import EditVideoForm from '@/components/forms/edit-video'
 
-type Props = {
-    videoId: string
-    title: string
-    description: string
-}
+type Props = { title: string; description: string; videoId: string }
 
-const EditVideo = ({ videoId, title, description }: Props) => {
-    return (
-        <div>
-            <h1>Edit Video</h1>
-        </div>
-    )
+const EditVideo = ({ description, title, videoId }: Props) => {
+  return (
+    <Modal
+      title="Edit video details"
+      description="You can update your video details here!"
+      trigger={
+        <Button variant={'ghost'}>
+          <Edit className="text-[#6c6c6c]" />
+        </Button>
+      }
+    >
+      {/* <EditVideoForm
+        videoId={videoId}
+        title={title}
+        description={description}
+      /> */}
+    </Modal>
+  )
 }
 
 export default EditVideo
