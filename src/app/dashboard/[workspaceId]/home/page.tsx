083a2e1dc081f6f896c6463eafeea6ad35543fa6
AnirbanDey-9497 <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-05-04 23:44:22 -0400

    Integrated Wix in the codebase

--- src/app/dashboard/[workspaceId]/home/page.tsx
+++ src/app/dashboard/[workspaceId]/home/page.tsx
@@ -1,29 +1,13 @@
-'use client'
-
-import { getAllUserVideos } from '@/actions/workspace'
-import Videos from '@/components/global/videos'
-import { useQueryData } from '@/hooks/useQueryData'
+import { getWixContent } from '@/actions/workspace'
+import React from 'react'
 
 type Props = {
-    params: {
-        workspaceId: string
-    }
+    
 }
 
-const HomePage = ({ params: { workspaceId } }: Props) => {
-    return (
-        <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
-                <h2 className="text-2xl font-semibold text-white">Recent Videos</h2>
-                <p className="text-neutral-400">Your recently uploaded and shared videos</p>
-            </div>
-            <Videos 
-                workspaceId={workspaceId} 
-                folderId={workspaceId}
-                videosKey="workspace-videos" 
-            />
-        </div>
-    )
+const HomePage = async (props: Props) => {
+    const video= await getWixContent()
+    return <div>HomePage</div>
 }
 
-export default HomePage 
+export default HomePage
